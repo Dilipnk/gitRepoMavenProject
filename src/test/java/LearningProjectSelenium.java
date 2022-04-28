a4827b0c4e1d714549ce8afdf2eb483db4e6fca0
TesterDilip1 <gtesterdilip1@gmail.com>
2022-04-28 12:49:43 +0530

    New project created

---
+++ src/test/java/LearningProjectSelenium.java
@@ -0,0 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LearningProjectSelenium {
+
+    WebDriver driver=null;
+    @BeforeSuite
+    public void openBrowser(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(chromeOptions);
+    }
+
+    @Test
+    public void executionPart(){
+
+        driver.get("https://accounts.google.com/");
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys("Test.test@gmail.com");
+
+    }
+
+    @AfterSuite
+    public void tearDown(){
+        driver.quit();
+    }
+}
